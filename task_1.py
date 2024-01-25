15251993441874d63d0d74a07adfcf9b207da3dd
bohdin <117035076+bohdin@users.noreply.github.com>
2024-01-25 21:43:09 +0200

    add task 2

--- task_1.py
+++ task_1.py
@@ -1,14 +1,17 @@
-import datetime as dt
+from datetime import datetime as dt
 
 def get_days_from_today(date):
+    # Перетворюємо рядом у об'єкт datetime, якщо формат не вірний повертаємо None
     try:
-        entered_date = dt.datetime.strptime(date, '%Y-%m-%d')
+        entered_date = dt.strptime(date, '%Y-%m-%d')
     except:
         print("You enter the date inncorrectly.\nDate string in 'YYYY-MM-DD' format")
         return
 
-    current_date = dt.datetime.now().date()
+    # Отримуємо поточну дату
+    current_date = dt.now().date()
 
+    # Рахуємо різницю в днях між введеною датою та поточною
     difference_days =  entered_date.toordinal() - current_date.toordinal()
     return difference_days
 
---
+++ task_2.py
@@ -0,0 +1,21 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    # Перевіряємо вхідні параметри
+    if min < 1 or max > 1000 or quantity > max or quantity < min:
+        return [] # Повертаємо пустий список, якщо вхідні параметри неправильні
+    else:
+        # Створюємо пусту множину
+        my_list = set()
+
+        # Додаємо числа поки їх не буде потрібної кількості
+        while(len(my_list) != quantity):
+            my_list.add(random.randint(min, max))
+
+        # З множини робимо список і сортуємо його
+        my_list = list(my_list)
+        my_list.sort()
+        return my_list
+    
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
